571ba13e10951a0f97288799953eded37be6c545
Denis-Wanjohi <deniswanjohi15@gmail.com>
2025-02-15 18:28:57 +0300

    Facial system - routes  filtering

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,17 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import { useUserStore } from '@/stores/user';
 import { useLecturerStore } from '@/stores/lecturer';
+// import DefaultView from '@/views/DefaultView.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'Default',
+      component: ()=> import('../views/DefaultView.vue'),
+    },
+    {
+      path: '/home',
       name: 'home',
       component: HomeView,
     },
@@ -91,21 +97,38 @@ const router = createRouter({
   ],
 })
 router.beforeEach((to, from, next) => {
+  let user;
   if(sessionStorage.getItem('lecturer')){
+    user = "lecturer"
+    useUserStore().setUserType = "lecturer"
     useLecturerStore().setLecturer(JSON.parse(sessionStorage.getItem('lecturer')))
   }
   if(sessionStorage.getItem('student')){
+    user = "student"
+    useUserStore().setUserType = "student"
     useUserStore().setUser(JSON.parse(sessionStorage.getItem('student')))
   }
 
 
   const token = sessionStorage.getItem('token');
   const publicPages = [
+            '/',
             '/login',
             '/register',
             '/lecturer/login',
             '/student/login',
         ];
+
+  const studentPages = [
+    '/student/dashboard',
+    '/student/login'
+  ]
+  const lecturerPages = [
+    '/lecturer/login',
+    '/dashboard/lecturer',
+    '/unit/:id',
+    '/class/setup'
+  ]
   const authRequired = !publicPages.includes(to.path);
   if(token != null){
     useUserStore().setLoggedIn(true);
@@ -113,9 +136,36 @@ router.beforeEach((to, from, next) => {
   const isAuthenticated = useUserStore().isLoggedIn && token;
 
   if (authRequired && !isAuthenticated) {
-    return next('/login');
-  } else if (isAuthenticated && publicPages.includes(to.path)) {
-    return next('/dashboard');
+    return next('/');
+  } else if(isAuthenticated){
+    if(publicPages.includes(to.path)){
+      if(user == "lecturer"){
+        console.log("lecturer goinh ouy")
+        return next("/dashboard/lecturer")
+      }else if(user == "student"){
+        console.log("lecturer goinh ouy")
+        return next("/student/dashboard")
+      }else{
+        return next('/dashboard');
+      }
+    }else{
+      console.log(user)
+      // checks if the user is actully accessing own routes
+      if(user == "lecturer"){
+        if(!lecturerPages.includes(to.path)){
+          return next("/dashboard/lecturer")
+        }else{
+            return next()
+        }
+      }else if(user == "student"){
+        if(!studentPages.includes(to.path)){
+          return next("/student/dashboard")
+        }else{
+            return next()
+        }
+      }
+    }
+
   }
 
   next()
