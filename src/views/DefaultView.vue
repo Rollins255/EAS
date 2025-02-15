e165438bb5264a674e724f7c13ceaa84b261bf2c
Denis-Wanjohi <deniswanjohi15@gmail.com>
2025-02-15 18:29:25 +0300

    Facial system - default page

---
+++ src/views/DefaultView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import router from '@/router';
+
+const choices = [
+    {
+        name:'A d m i n',
+        link:'/login'
+    },
+    {
+        name:'L e c t u r e r',
+        link:'/lecturer/login'
+    },
+    {
+        name:'S t u d e n t',
+        link:'/student/login'
+    }
+]
+</script>
+
+<template>
+    <div class="sm:flex justify-around items-center h-screen">
+        <div class="bg-blue-500 rounded shadow-lg cursor-pointer" v-for="item in choices" @click.prevent="router.push(`${item.link}`)" >
+            <p class="p-10 text-center text-4xl font-bold  m-5">{{item.name.toUpperCase()}}</p>
+        </div>
+    </div>
+</template>
