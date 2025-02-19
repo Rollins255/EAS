ec4796d682cfc518c1fb1d5eb74c4b3d509b1a1c
Denis-Wanjohi <deniswanjohi15@gmail.com>
2025-02-19 22:21:40 +0300

    Facial system

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL:'https://c3c2-102-213-241-212.ngrok-free.app/api'
+    baseURL:'https://7036-102-215-12-244.ngrok-free.app/api'
     // baseURL:'http://localhost:8000/api'
 })
 axiosClient.interceptors.request.use(config =>{
