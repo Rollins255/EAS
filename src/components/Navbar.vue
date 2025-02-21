0de989030d03467406371c4d69bf55bac46a2f5b
Denis-Wanjohi <deniswanjohi15@gmail.com>
2025-02-21 14:46:59 +0300

    Facial system - class setup button

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,10 +3,15 @@
     <div class="w-full flex justify-between h-[80px] bg-black text-white items-center px-10">
             <div class="font-bold md:text-3xl"> <RouterLink to="/">CLASS ATTENDACE</RouterLink> </div>
             <div class="hidden md:block">
+                <Button v-if="user !== null" @click="router.push('/class/setup')" class="text-sm font-bold mx-5">C l a s s</Button>
                 <Button
                     @click="logout"
-                    v-if="token != null"
+                    class="text-sm font-bold"
+                    v-if="token != null && !isLogOut"
                 >Logout</Button>
+                <Button v-else  type="button" class="my-auto text-nowrap">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="white"><circle cx="12" cy="3.5" r="2"><animateTransform attributeName="transform" calcMode="discrete" dur="2.4s" repeatCount="indefinite" type="rotate" values="0 12 12;90 12 12;180 12 12;270 12 12"/><animate attributeName="opacity" dur="0.6s" repeatCount="indefinite" values="1;1;0"/></circle><circle cx="12" cy="3.5" r="1.5" transform="rotate(30 12 12)"><animateTransform attributeName="transform" begin="0.2s" calcMode="discrete" dur="2.4s" repeatCount="indefinite" type="rotate" values="30 12 12;120 12 12;210 12 12;300 12 12"/><animate attributeName="opacity" begin="0.2s" dur="0.6s" repeatCount="indefinite" values="1;1;0"/></circle><circle cx="12" cy="3.5" r="1.5" transform="rotate(60 12 12)"><animateTransform attributeName="transform" begin="0.4s" calcMode="discrete" dur="2.4s" repeatCount="indefinite" type="rotate" values="60 12 12;150 12 12;240 12 12;330 12 12"/><animate attributeName="opacity" begin="0.4s" dur="0.6s" repeatCount="indefinite" values="1;1;0"/></circle></g></svg>
+                </Button>
             </div>
             <div class="md:hidden">
                 <!-- {{ user == null }} -->
@@ -44,6 +49,7 @@ import { useToast } from "primevue/usetoast";
 const user = sessionStorage.getItem('lecturer')
 const confirm = useConfirm();
 const toast = useToast();
+const isLoggingOut = ref(false)
 const token = ref()
 const isLogOut = ref(false)
 const visible = ref(false)
@@ -97,6 +103,10 @@ const logout = ()=>{
         isLogOut.value = false
         // alert('please logout again')
     })
+}
+
+function classSetup(){
+    router.push('/class/setup')
 }
 </script>
 
