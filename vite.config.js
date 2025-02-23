66f3e1155b893e90fab119202c56e871f27ffb5f
Denis-Wanjohi <deniswanjohi15@gmail.com>
2025-02-23 23:21:00 +0300

    Facial system - dockerization

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,26 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+globalThis.structuredClone = globalThis.structuredClone || (obj => JSON.parse(JSON.stringify(obj)));
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+    }
   },
   define: {
     'import.meta.env.TEST': JSON.stringify(process.env.NODE_ENV === 'test'),
