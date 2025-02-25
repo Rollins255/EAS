bc9a4231ad87c5c0da7e96337dd766c32401eeb0
Denis-Wanjohi <deniswanjohi15@gmail.com>
2025-02-25 10:01:56 +0300

    Facial system - remove facials column

--- api/database/migrations/2025_02_03_063446_create_students_table.php
+++ api/database/migrations/2025_02_03_063446_create_students_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('regNo')->unique();
             $table->string('idNo');
-            $table->unsignedBigInteger('facials')->nullable();
+            // $table->unsignedBigInteger('facials')->nullable();
             $table->unsignedBigInteger('faculty');
             $table->unsignedBigInteger('department');
             $table->unsignedBigInteger('course');
