60f2c821b45037e4431c2fef464de40226794165
Denis-Wanjohi <deniswanjohi15@gmail.com>
2025-02-25 10:11:53 +0300

    Facial system - default admin details

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -6,9 +6,12 @@ use App\Models\Admin;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
+    protected static ?string $password;
     /**
      * Seed the application's database.
      */
@@ -21,6 +24,13 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        Admin::factory(5)->create();
+        // Admin::factory(5)->create();
+        Admin::factory()->create([
+            'name' => fake()->name(),
+            'email' => 'test@example.com',
+            'idNo' => 00000000,
+            'staffNo' => 'CU000',
+            'password' => static::$password ??= Hash::make('00000000'),
+        ]);
     }
 }
